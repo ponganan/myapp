acb0dd61d99319996698c07cb4636aa90af7e1a0
ponganan <joeyubu@gmail.com>
2022-11-23 23:18:52 +0700

    add product_image.dart

---
+++ lib/pages/management/widgets/product_image.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductImage extends StatefulWidget {
+  const ProductImage({Key? key}) : super(key: key);
+
+  @override
+  State<ProductImage> createState() => _ProductImageState();
+}
+
+class _ProductImageState extends State<ProductImage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
